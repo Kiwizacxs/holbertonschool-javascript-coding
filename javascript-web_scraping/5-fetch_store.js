10c5d4a13a3481edd11796459253b75a63acdd95
Kiwizacxs <brian.deleon2016@outlook.com>
2024-07-09 18:01:46 -0700

    fetch store

---
+++ javascript-web_scraping/5-fetch_store.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+if (!url || !filePath) {
+  console.error('Usage: ./5-fetch_store.js <URL> <file-path>');
+  process.exit(1);
+}
+
+request.get(url, (error, response, body) => {
+  if (error) {
+      console.error('Error:', error);
+      return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error(`Failed to retrieve data. Status code: ${response.statusCode}`);
+    return;
+  }
+
+  fs.writeFile(filePath, body, 'utf-8', (err) => {
+    if (err) {
+      console.error('Error writing file:', err);
+      return;
+    }
+    console.log(`Successfully wrote response body to ${filePath}`);
+  });
+});
