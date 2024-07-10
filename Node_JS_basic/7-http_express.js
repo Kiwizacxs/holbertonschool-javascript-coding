def05c6c1a3e8c26385b7573c3c2accaaa5d348a
Kiwizacxs <brian.deleon2016@outlook.com>
2024-07-10 09:45:44 -0700

    seven

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const csv = require('csv-parser');
-const results = [];
 
 app.use(express.text());
 
@@ -11,15 +9,17 @@ app.get('/', (req, res) => {
 });
 
 app.get('/students', (req, res) => {
-  fs.createReadStream(process.argv[2])
-    .pipe(csv())
-    .on('data', (data) => results.push(data))
-    .on('end', () => {
-      const students = results.filter((student) => student.Field !== '');
-      const csStudents = students.filter((student) => student.Field === 'CS').map((student) => student.First);
-      const sweStudents = students.filter((student) => student.Field === 'SWE').map((student) => student.First);
+  fs.readFile(process.argv[2], 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error reading file');
+    } else {
+      const students = data.split('\n').map((line) => line.split(',')).filter((student) => student[0]!== '');
+      const csStudents = students.filter((student) => student[1] === 'CS').map((student) => student[0]);
+      const sweStudents = students.filter((student) => student[1] === 'SWE').map((student) => student[0]);
       res.send(`This is the list of our students\nNumber of students: ${students.length}\nNumber of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}\nNumber of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
-    });
+    }
+  });
 });
 
 app.listen(1245, () => {
