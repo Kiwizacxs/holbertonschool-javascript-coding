95836e5bcb2db193ee7d6e889b88e54f79acffac
Kiwizacxs <brian.deleon2016@outlook.com>
2024-07-09 17:49:42 -0700

    numbre of movies

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,8 +1,13 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
 const request = require('request');
 
 const apiUrl = process.argv[2];
+
+if (!apiUrl) {
+  console.error('Usage: ./4-starwars_count.js <API URL>');
+  process.exit(1);
+}
 
 request.get(apiUrl, (error, response, body) => {
   if (error) {
@@ -15,15 +20,25 @@ request.get(apiUrl, (error, response, body) => {
     return;
   }
 
-  const films = JSON.parse(body).results;
-  const wedgeAntillesUrl = 'http://swapi-api.hbtn.io/api/people/18/';
-    
+  let films;
+  try {
+    films = JSON.parse(body).results;
+  } catch (parseError) {
+    console.error('Failed to parse JSON:', parseError);
+    return;
+  }
+
+  if (!Array.isArray(films)) {
+    console.error('Unexpected API response format');
+    return;
+  }
+
   const wedgeAntillesUrls = [
     'https://swapi-api.hbtn.io/api/people/18/',
     'http://swapi-api.hbtn.io/api/people/18/',
-      'http://localhost:5050/people/18/',
-      'https://localhost:5050/people/18/'
-    ];
+    'http://localhost:5050/people/18/',
+    'https://localhost:5050/people/18/'
+  ];
 
   const count = films.reduce((acc, film) => {
     if (film.characters.some(url => wedgeAntillesUrls.includes(url))) {
@@ -31,6 +46,6 @@ request.get(apiUrl, (error, response, body) => {
     }
     return acc;
   }, 0);
-  
+
   console.log(count);
 });
