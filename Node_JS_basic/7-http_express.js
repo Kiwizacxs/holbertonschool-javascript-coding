bd3f3fccb3fc0e073adfcf208f746446eb46ad74
Kiwizacxs <brian.deleon2016@outlook.com>
2024-07-10 09:20:36 -0700

    seven

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,29 +1,39 @@
 const express = require('express');
-
-const args = process.argv.slice(2);
-const countStudents = require('./3-read_file_async');
-
-const DATABASE = args[0];
+const fs = require('fs').promises;
 
 const app = express();
-const port = 1245;
+const PORT = 1245;
 
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+  res.type('text').send('Hello Holberton School!\n');
 });
-
 app.get('/students', async (req, res) => {
-  const msg = 'This is the list of our students\n';
   try {
-    const students = await countStudents(DATABASE);
-    res.send(`${msg}${students.join('\n')}`);
+    const data = await fs.readFile(process.argv[2], 'utf8');
+    const lines = data.split('\n').filter(line => line.trim() !== '');
+    const counters = {};
+
+    lines.slice(1).forEach(line => {
+      const [, firstName, , field] = line.split(',');
+      if (field) {
+        counters[field] = counters[field] || { count: 0, names: [] };
+        counters[field].count++;
+        counters[field].names.push(firstName.trim());
+      }
+    });
+    let response = 'This is the list of our students\n';
+    response += `Number of students: ${lines.length - 1}\n`;
+
+    for (const field in counters) {
+      response += `Number of students in ${field}: ${counters[field].count}. List: ${counters[field].names.join(', ')}\n`;
+    }
+    res.type('text').send(response);
   } catch (error) {
-    res.send(`${msg}${error.message}`);
+    res.status(500).send('Cannot load the database');
   }
 });
-
-app.listen(port, () => {
-  //   console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
 
 module.exports = app;
