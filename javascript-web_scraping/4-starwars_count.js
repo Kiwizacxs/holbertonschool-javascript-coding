ee66a611aeb0b966d56423477c4ac84ff1a50476
Kiwizacxs <brian.deleon2016@outlook.com>
2024-07-09 17:58:54 -0700

    numbre of movies

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,36 +1,22 @@
 #!/usr/bin/node
 
 const request = require('request');
+const url = process.argv[2];
+let count = 0;
 
-const apiUrl = process.argv[2];
-
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
     return;
   }
-
-  if (response.statusCode !== 200) {
-    console.error(`Failed to retrieve data. Status code: ${response.statusCode}`);
-    return;
-  }
-
   const films = JSON.parse(body).results;
-  const wedgeAntillesUrl = 'http://swapi-api.hbtn.io/api/people/18/';
-    
-  const wedgeAntillesUrls = [
-    'https://swapi-api.hbtn.io/api/people/18/',
-    'http://swapi-api.hbtn.io/api/people/18/',
-      'http://localhost:5050/people/18/',
-      'https://localhost:5050/people/18/'
-    ];
-
-  const count = films.reduce((acc, film) => {
-    if (film.characters.some(url => wedgeAntillesUrls.includes(url))) {
-      acc += 1;
+  for (const film of films) {
+    for (const character of film.characters) {
+      if (character.endsWith('/18/')) {
+        count++;
+      }
     }
-    return acc;
-  }, 0);
-  
+  }
+
   console.log(count);
 });
