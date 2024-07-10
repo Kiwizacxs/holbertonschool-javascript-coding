947dbb188c4bc00e87c5d3b5839f7207613d64c2
Kiwizacxs <brian.deleon2016@outlook.com>
2024-07-10 08:51:13 -0700

    the one

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+process.stdin.setEncoding('utf8');
+
+console.log("Welcome to Holberton School, what is your name?");
+
+process.stdin.on('data', (input) => {
+    const name = input.trim();
+    console.log(`Your name is: ${name}`);
+    process.exit();
+});
+
+process.on('exit', () => {
+    console.log("This important software is now closing");
+});
